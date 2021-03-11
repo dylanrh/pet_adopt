082c37e33a0815c690cd0667181f913db2217057
Dylan Hensel <dylan@hensel.com>
2021-03-11 10:04:01 -0800

    fixes babel files and transpilation and adds JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from "react";
+// import React from "react"; no longer need this since Babel understands we need React
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+// const App = () => {
+//   return React.createElement("div", { id: "my-brand" }, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Buddy Holly",
+//       animal: "Snuggle Bug",
+//       breed: "Boston Terrier mix",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Mac",
+//       animal: "Good boy",
+//       breed: "Doberman",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Ren",
+//       animal: "Baby boy",
+//       breed: "Papillon",
+//     }),
+//   ]);
+// };
+
+//below does the same as above under the hood / when transpiled
 const App = () => {
-  return React.createElement("div", { id: "my-brand" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Buddy Holly",
-      animal: "Snuggle Bug",
-      breed: "Boston Terrier mix",
-    }),
-    React.createElement(Pet, {
-      name: "Mac",
-      animal: "Good boy",
-      breed: "Doberman",
-    }),
-    React.createElement(Pet, {
-      name: "Ren",
-      animal: "Baby boy",
-      breed: "Papillon",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Buddy Holly" animal="Snuggle Bug" breed="Boston Terrier mix" />
+      <Pet name="Mac" animal="Good boy" breed="Doberan" />
+      <Pet name="Ren" animal="Baby boy" breed="Papillon" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
+
+// the below code is compiled into what is above
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
