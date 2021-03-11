39490fc94c67aa234462feb249760fda1a396944
Dylan Hensel <dylan@hensel.com>
2021-03-11 08:49:10 -0800

    set up prettier and npm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Buddy Holly"),
-    React.createElement("h3", {}, "Snuggle bug"),
-    React.createElement("h3", {}, "Boston Terrier Mix"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
+  return React.createElement("div", { id: "my-brand" }, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Buddy Holly",
+      animal: "Snuggle Bug",
+      breed: "Boston Terrier mix",
+    }),
+    React.createElement(Pet, {
+      name: "Mac",
+      animal: "Good boy",
+      breed: "Doberman",
+    }),
+    React.createElement(Pet, {
+      name: "Ren",
+      animal: "Baby boy",
+      breed: "Papillon",
+    }),
   ]);
 };
 
