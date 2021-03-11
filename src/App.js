d187508cda263ec735ad224ee9478498841cb826
Dylan Hensel <dylan@hensel.com>
2021-03-11 11:23:14 -0800

    adds SearchParams component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import React from "react"; no longer need this since Babel understands we need React
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 // const App = () => {
 //   return React.createElement("div", { id: "my-brand" }, [
@@ -28,9 +29,7 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Buddy Holly" animal="Snuggle Bug" breed="Boston Terrier mix" />
-      <Pet name="Mac" animal="Good boy" breed="Doberan" />
-      <Pet name="Ren" animal="Baby boy" breed="Papillon" />
+      <SearchParams />
     </div>
   );
 };
